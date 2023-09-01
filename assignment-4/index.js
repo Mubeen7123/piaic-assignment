1be89eb06b199f92c7a50d25d8df262d4c43920c
Mubeen7123 <HLATAX111@gmail.com>
2023-09-01 22:37:21 +0500

    first commit

---
+++ assignment-4/index.js
@@ -0,0 +1,82 @@
+"use strict";
+////1.create a function that takes an array an index and a value as perameters. inside the functon use the splice method to insert the value  at the specified index in the  array. return the modified array.
+// let array:number[] = [1,2,3,4,6,8];
+// let index:number = 5;
+// let value:number = 7;
+// function arrayIndexValue(array:number [],index:number,value:number)
+// {
+//     array.splice (index,0,value)
+//     return array
+// }
+// var modifyArray:number[]=arrayIndexValue(array,index,value)
+// console.log(modifyArray);
+////
+//2.implement a simple shopping cart program using an array. Create functions to add items, remove items, and update quantities using the splice method. Print the cart's contents after each operation
+//Create functions to add items
+// let shoppingAddCart:string[] = ["Fruits","Vegitable","stationary"]
+// shoppingAddCart.splice(2,0,"eggs");
+// console.log("add Item",shoppingAddCart);
+// //Create functions to remove items
+// let shoppingRemovecart:number[]= [15,1122,103,1218]
+// shoppingRemovecart.splice(0,1,111)
+// console.log("remove",shoppingRemovecart);
+// //Create functions to update quantities 
+// let updateQuantities:string[]= ["chair","table","fan","oven,LCD"]
+// updateQuantities.splice(4,0,"wall clock","lights")
+// console.log("update",updateQuantities);
+//// 3.Write a program that uses a while loop to print the first 25 integers.
+// let num:number = 1;
+// while (num<=25) {
+//     console.log(num);
+//     num++
+// }
+// ////4.Write a program that uses a while loop to print the first 10 even numbers
+// let i:number = 1;
+// while (i<=10) {
+//  let result:number = i *2;
+// console.log(result);
+// i++
+// }
+////Create a function that takes a positive integer as parameter and uses a while loop to calculate and return the factorial of that number.
+// function integar(n:number) {
+//     if (n<0) {
+//         console.log("Factorial is not define for -v numbers");
+//     }
+//     else if (n==0) {
+//         console.log("1");
+//     }
+//     else{
+//     }
+// } 
+//Write a program having an array of numbers if the number is negative it should remove the negative number from the array.
+let indexArray = [-3, -1, -1, 1, -2, 3, 4, 5,];
+console.log(`all arrays number ${indexArray}`);
+for (let i = 0; i < indexArray.length; i++) {
+    if (indexArray[i] < 0) {
+        indexArray.splice(i, 1);
+    }
+}
+console.log(indexArray);
+////Create a function that takes an array of numbers as parameter. Use a while loop to calculate and return the sum of all the numbers in the array.
+let index = [5, 10, 15, 20, 25, 30,];
+function sum(num) {
+    let i = 0;
+    let sum = 0;
+    while (i < index.length) {
+        sum = sum + index[i];
+        i++;
+    }
+    return sum;
+}
+//// Implement a program that takes a list of temperatures in Celsius as input from the user. Convert each temperature to Fahrenheit using the formula F = (C * 9/5) + 32 and store the converted temperatures in an array. Use a while loop to perform the conversion for each temperature.
+let temperature = [20, 30, 40, 50, 60];
+var fahrenhiet = [];
+function InFahreniet(temperature, fahrenhiet) {
+    let i = 0;
+    while (i < temperature.length) {
+        fahrenhiet[i] = (temperature[i] * 9 / 5) + 32;
+        i++;
+    }
+}
+InFahreniet(temperature, fahrenhiet);
+console.log(`Temprature list After Conversion ${fahrenhiet}`);
---
+++ assignment-6/index.js
@@ -0,0 +1,46 @@
+"use strict";
+//*write a program that uses a filter to remove all negative numbers from an array
+let mixarry = [-1, 2, -3, 4, -5, 6, -7, 8, -9, 10];
+let positivearry = mixarry.filter((element) => {
+    if (element >= 0) {
+        return element;
+    }
+});
+console.log(positivearry);
+//* Give an array of numbers [1,2,3,4,5] use the map mathods to create a new array where each number is multiplied by 2.
+let arr = [1, 2, 3, 4, 5];
+let multipliedArr = arr.map((element) => {
+    return element * 2;
+});
+console.log(`multiplied array ${multipliedArr}`);
+//*Give an array of temprature in celcious [0,10,20,30,40]use the map method to createa new arry where each temprature is converted to fohrenheit usig the formula(celcious * 9/5)+32;
+let tempratureCelcious = [0, 10, 20, 30, 40];
+let fohrenheit = tempratureCelcious.map((celcious) => {
+    return celcious * (9 / 5) + 32;
+});
+console.log(fohrenheit);
+//*Give an array of strings ["Apple","Banana","Cherry","Date","Grape",] use the filter method to create a new array containing only the fruits with more than 5 characters.
+let fruits = ["Apple", "banana", "cherry", "date", "grape"];
+let fruitsabove5Character = fruits.filter((elements) => {
+    if (elements.length > 5) {
+        return elements;
+    }
+});
+console.log(fruitsabove5Character);
+//* Given an array of numbers[1,2,3,4,5,6,7,8,9,10] use the map and filter methods together to create a new array contining the squares of even numbers
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let evenarray = array.map((elements) => elements ** 2).filter((elements) => {
+    if (elements % 2 == 0) {
+        return elements;
+    }
+});
+console.log(evenarray);
+//*given an array of numbers [3,6,9,12,15,18],use the map and filter methods together to create a new array conatining the double values of odd numbers.
+let OddArray = [3, 6, 9, 12, 15, 18];
+let oddDoubleArray = OddArray.filter((element) => element % 2 !== 0).map((element) => element * 2, {});
+console.log(oddDoubleArray);
+//*Give an array of names ["Alice","Bob","Charlie","Devid","Emily"],use the forEach Method to log each name with an exclamation mark at the end. e.g., "Alice!".
+let names = ["Alice", "Bob", "Charlie", "Devid", "Emily"];
+names.forEach(function (a) {
+    console.log(a + "!");
+});
